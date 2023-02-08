c41e7f36f40916eccf72d787c0b3d20ae535e8b5
Martin Kersner <martin@bisonai.com>
2023-02-08 14:35:26 +0900

    feat: Auxiliary script to read account settings

---
+++ scripts/get-account.ts
@@ -0,0 +1,26 @@
+import { ethers } from 'hardhat'
+import { Prepayment__factory } from '@bisonai-cic/icn-contracts'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function main() {
+  const ACC_ID = process.env.ACC_ID
+  if (!ACC_ID) {
+    console.error('ACC_ID not defined in .env file')
+  } else {
+    const { prepayment: prepaymentAddress } = await hre.getNamedAccounts()
+    const prepayment = await ethers.getContractAt(Prepayment__factory.abi, prepaymentAddress)
+
+    const { balance, reqCount, owner, consumers } = await prepayment.getAccount(ACC_ID)
+    console.log(`balance:\t${balance}`)
+    console.log(`reqCount:\t${reqCount}`)
+    console.log(`owner:\t\t${owner}`)
+    console.log(`consumers:\t${consumers}`)
+  }
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
