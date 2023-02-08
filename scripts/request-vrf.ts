be3affe973c2c2d84704a5c8aa60f5f6d47ee114
Martin Kersner <martin@bisonai.com>
2023-02-08 11:15:06 +0900

    feat: Load account ID from .env

--- scripts/request-vrf.ts
+++ scripts/request-vrf.ts
@@ -1,14 +1,21 @@
 import { ethers } from 'hardhat'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 async function main() {
-  const vrfConsumer = await ethers.getContract('VRFConsumer')
+  const ACC_ID = process.env.ACC_ID
+  if (!ACC_ID) {
+    console.error('ACC_ID not defined in .env file')
+  } else {
+    const vrfConsumer = await ethers.getContract('VRFConsumer')
 
-  const keyHash = '0x47ede773ef09e40658e643fe79f8d1a27c0aa6eb7251749b268f829ea49f2024'
-  const accId = 1
-  const callbackGasLimit = 500_000
-  const numWords = 1
+    const keyHash = '0x47ede773ef09e40658e643fe79f8d1a27c0aa6eb7251749b268f829ea49f2024'
+    const callbackGasLimit = 500_000
+    const numWords = 1
 
-  await vrfConsumer.requestRandomWords(keyHash, accId, callbackGasLimit, numWords)
+    await vrfConsumer.requestRandomWords(keyHash, ACC_ID, callbackGasLimit, numWords)
+  }
 }
 
 main().catch((error) => {
