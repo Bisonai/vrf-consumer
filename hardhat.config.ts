fb0172c1b25c5befffe789711692222e335a1379
bayram98 <bayramguwanjow@gmail.com>
2023-04-26 15:10:44 +0900

    feat: add hardhat deposit task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import '@nomiclabs/hardhat-web3'
 import '@nomiclabs/hardhat-ethers'
 import 'hardhat-deploy'
 import dotenv from 'dotenv'
+import { Prepayment__factory } from '@bisonai/orakl-contracts'
 
 dotenv.config()
 
@@ -68,6 +69,27 @@ task('address', 'Convert mnemonic to address')
   .setAction(async (taskArgs, hre) => {
     const wallet = hre.ethers.Wallet.fromMnemonic(taskArgs.mnemonic)
     console.log(wallet.address)
+  })
+
+task('deposit', 'Deposit klay to AccId')
+  .addParam('amount', 'The amount of Klay')
+  .setAction(async (taskArgs, hre) => {
+    const accId = process.env.ACC_ID
+    const klayAmount = taskArgs.amount
+
+    if (accId) {
+      const { prepayment: prepaymentAddress } = await hre.getNamedAccounts()
+      const prepayment = await ethers.getContractAt(Prepayment__factory.abi, prepaymentAddress)
+      const amount = ethers.utils.parseEther(klayAmount)
+      const txReceipt = await (await prepayment.deposit(accId, { value: amount })).wait()
+      const balance = txReceipt.events[0].args.newBalance.toString()
+      const newBalance = ethers.utils.formatEther(balance)
+
+      console.log(`Deposited ${klayAmount} KLAY to account Id ${accId}`)
+      console.log(`New balance: ${newBalance} klay`)
+    } else {
+      console.log(`Prepayment account ID is not defined`)
+    }
   })
 
 export default config
