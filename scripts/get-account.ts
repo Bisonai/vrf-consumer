a72b60cb6813c408d7b19edb3ced34e8fa8c1a12
bayram98 <bayramguwanjow@gmail.com>
2023-06-30 18:19:25 +0900

    feat: show balance in Klay

--- scripts/get-account.ts
+++ scripts/get-account.ts
@@ -13,7 +13,9 @@ async function main() {
     const prepayment = await ethers.getContractAt(Prepayment__factory.abi, prepaymentAddress)
 
     const { balance, reqCount, owner, consumers } = await prepayment.getAccount(ACC_ID)
+    const amountKlay = balance / Number(ethers.utils.parseEther('1.0'))
     console.log(`balance:\t${balance}`)
+    console.log(`balance:\t${amountKlay} Klay`)
     console.log(`reqCount:\t${reqCount}`)
     console.log(`owner:\t\t${owner}`)
     console.log(`consumers:\t${consumers}`)
