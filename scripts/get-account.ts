c163c0b2c2dadab24859f35421b7c4558fe4a51f
bayram98 <bayramguwanjow@gmail.com>
2023-07-13 15:14:17 +0900

    fix: increase fund klay amount

--- scripts/get-account.ts
+++ scripts/get-account.ts
@@ -13,8 +13,7 @@ async function main() {
     const prepayment = await ethers.getContractAt(Prepayment__factory.abi, prepaymentAddress)
 
     const { balance, reqCount, owner, consumers } = await prepayment.getAccount(ACC_ID)
-    const amountKlay = balance / Number(ethers.utils.parseEther('1.0'))
-    console.log(`balance:\t${balance}`)
+    const amountKlay = ethers.utils.formatUnits(balance, 'ether')
     console.log(`balance:\t${amountKlay} Klay`)
     console.log(`reqCount:\t${reqCount}`)
     console.log(`owner:\t\t${owner}`)
